eb8a6cccf873624bdadb367c8e04fea4294e7e70
yongliu <a3831524@126.com>
2017-08-30 22:53:03 +0800

    aaaa

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -14,7 +14,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
 
 class TaskController extends Controller
 {
@@ -22,7 +24,7 @@ class TaskController extends Controller
      * @Route("Task/new")
      */
     public function newAction(Request $request)
-    {ç
+    {
         // create a task and give it some dummy data for this example
         $task = new Task();
         $task->setTask('Write a blog post');
@@ -37,5 +39,35 @@ class TaskController extends Controller
         return $this->render('task/new.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * @Route("Task/test")
+     */
+ function testAction()
+ {
+     // make a real request to an external site
+     $client = new Client();
+     $crawler = $client->request('GET', ' http://localhost/test/c.php');
+
+// select the form and fill in some values
+     $form = $crawler->selectButton('Log in')->form();
+     $form['login'] = 'symfonyfan';
+     $form['password'] = 'anypass';
+
+// submit that form
+     $crawler = $client->submit($form);
+    var_dump($crawler);
+ }
+
+    function tttAction()
+    {
+        $loader = new FilesystemLoader(__DIR__ . '/views/%name%');
+
+        $templating = new PhpEngine(new TemplateNameParser(), $loader);
+
+        echo $templating->render('hello.php', array('firstname' => 'Fabien'));
+
+
     }
 }
